39c982a69f38ac6fdf1a30c75c81d8ab62d24ccc
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-04-18 06:14:20 -0700

    File storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+import models
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in the storage"""
+        if not cls:
+            return FileStorage.__objects
+        else:
+            newdict = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    newdict[key] = value
+            return newdict
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        temp = {}
+        temp.update(FileStorage.__objects)
+        for key, val in temp.items():
+            temp[key] = val.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(temp, f)
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        try:
+            with open(FileStorage.__file_path, "r", encoding="UTF8") as fd:
+                for val in json.load(fd).values():
+                    class_name = val["__class__"]
+                    del val["__class__"]
+                    self.new(eval(class_name)(**val))
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects"""
+        if obj:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
