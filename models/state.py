657c44c6dcbf2926bde26c24b0626ef08cdd5618
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-04-19 15:21:31 -0700

    model test

---
+++ models/state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import models
+
+
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                            cascade="all, delete, delete-orphan")
+
+    if getenv('HBNB_TYPE_STORAGE') != "db":
+        @property
+        def cities(self):
+            """ returns list of City instances related to state """
+            from models import storage
+            list_cities = []
+            for city in storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
