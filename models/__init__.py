0ec2355f3c9c46cf9272989b4804771c56b6e0b3
Olayinka Ajibabi <yinka.ajibabi@gmail.com>
2023-04-19 15:14:08 -0700

    Models

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''The Package initializer'''
+from os import getenv
+
+if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+storage.reload()
